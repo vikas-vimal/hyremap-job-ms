623db910e559800fb2a1cc56087aad825ad9c11c
Vikki <vikas@hood.live>
2024-04-12 12:42:01 +0530

    Blank spring boot application created with application properties

---
+++ src/main/java/com/designfox/hyremapjobms/HyremapJobMsApplication.java
@@ -0,0 +1,13 @@
+package com.designfox.hyremapjobms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyremapJobMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyremapJobMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designfox/hyremapjobms/HyremapJobMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designfox.hyremapjobms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyremapJobMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
